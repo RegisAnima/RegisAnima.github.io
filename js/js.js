dfe1a606de0194f11219d528e982c292c83b5649
Shokhrukh <57153368+RegisAnima@users.noreply.github.com>
2021-03-30 16:39:01 +0300

    END

---
+++ js/js.js
@@ -0,0 +1,10 @@
+/*! project-name v0.0.1 | (c) 2021 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
+$('.slider').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+});
+
+$(".menu-button").click((function () {
+    $(this).closest("body").toggleClass("active");
+}));
---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*! project-name v0.0.1 | (c) 2021 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
+// JavaScript Document
+ $(document).ready((function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  }));
